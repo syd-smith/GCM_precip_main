e7c9ea2c2be5f5aeb8a0a33a25978790d30497a6
Sydney Smith <smithsydneyn@gmail.com>
2025-12-08 21:54:28 -0700

    stats analysis

--- GCM_bias/bias_taylor.py
+++ GCM_bias/bias_taylor.py
@@ -60,13 +60,25 @@ def scaled_data(variable):
             pos_vals = values[pos_mask]
             scaled_bias[pos_mask] = pos_vals / b
             
+    elif variable == 'pr':
+        # Map values so that:
+         # - 1 in the original data maps to 0
+         # - extremes map to -1 and 1
+         a = values.min()
+         b = values.max()
+
+         # max distance from 1 on either side
+         scale = max(1 - a, b - 1)
+
+         scaled_bias = (values - 1.0) / scale
+            
     else:
         # linearly scale raw values into 0 to 1 range for arccos function  (returns np.array)
         scaled_bias = (values - a) / (b - a)
         
-    return a, b, scaled_bias
+    return a, b, scaled_bias, values
 
-# low, high, adjusted = scaled_data('pr')
+# low, high, adjusted, raw = scaled_data('pr')
     
 
 def bias_taylor(variable_list):
@@ -76,14 +88,14 @@ def bias_taylor(variable_list):
     obs_stdev_ratio = 1
 
     # Setup polar plot: angle = arccos(correlation coefficient), radius = standard deviation
-    fig, axs = bp.plt.subplots(1, 2, subplot_kw={'projection': 'polar'}, figsize = (10, 8))
+    fig, axs = bp.plt.subplots(1, 2, subplot_kw={'projection': 'polar'}, figsize = (11, 8))
     
     one, two = axs
     
-    pr_levels = bp.np.array([50, 60, 70, 80, 90, 100, 125, 150, 175, 200, 225])
+    pr_levels = bp.np.array([50, 65, 80, 95, 100, 105, 150, 190, 220])
     temp_levels = bp.np.array([3, 4, 5, 6, 7, 8, 9])
     
-    pr_norm = bp.mcolors.BoundaryNorm(pr_levels, ncolors = bp.plt.get_cmap(bp.cmap.cmap('BlRe'), 10).N) # 15 bins means 14 edges
+    pr_norm = bp.mcolors.BoundaryNorm(pr_levels, ncolors = bp.plt.get_cmap(bp.cmap.cmap('MPL_BrBG'), 10).N) # 15 bins means 14 edges
     ts_norm = bp.mcolors.BoundaryNorm(temp_levels, ncolors = bp.plt.get_cmap(bp.cmap.cmap('BlRe'), 6).N) # 15 bins means 14 edges
     
     for index, diagram in enumerate([one, two]):
@@ -97,21 +109,51 @@ def bias_taylor(variable_list):
         diagram.xaxis.grid(False)
 
         # Plot the reference (observation) point 
-        diagram.plot(bp.np.pi - bp.np.arccos(obs_bias), obs_stdev_ratio, 'k*', label = "Reference", markersize = 15)
+        diagram.plot(bp.np.pi - bp.np.arccos(obs_bias), obs_stdev_ratio, marker = 'o', markerfacecolor =  'none', markeredgecolor = 'k', label = "Reference", markersize = 12)
 
-        min_value, max_value, data = scaled_data(variable_list[index])
+        min_value, max_value, data, raw_data = scaled_data(variable_list[index])
         
         if min_value < 0:
             diagram.set_thetamin(0)
+            pad = -0.08
             
             # Draw lines for various correlation coefficients as grid
             bias_lines = bp.np.array([-1.0, -0.8, -0.6, -0.4, -0.2, 0, 0.2, 0.4, 0.6, 0.8, 1.0])
             min_label = bp.np.linspace(min_value, 0,  6)
             max_label = bp.np.linspace(0, max_value, 6)
             bias_labels = bp.np.concatenate([min_label[:-1], max_label])
+            
+            # add more stdev labels
+            r_ticks = [0.5, 1.0, 1.5, 2.0, 2.5]
+            
+            for r, move_it in zip(r_ticks, [0.05, 0.09, 0.13, 0.17, 0.21]):
+                # Default side (right/top, angle ~22.5° or wherever your main labels are)
+                # Add labels on the left (angle = À radians)
+                fig.text(0.24 + move_it, 0.119, f"{r}", ha = 'center', va = 'center', fontsize = 9.75, color =  'k')
+                fig.text(0.74 + move_it, 0.119, f"{r}", ha = 'center', va = 'center', fontsize = 9.75, color =  'k')
+
+        elif 'pr' in variable_list:
+            diagram.set_thetamin(0)
+            pad = -0.08
+            
+            # Draw lines for various correlation coefficients as grid
+            bias_lines = bp.np.array([-1.0, -0.8, -0.6, -0.4, -0.2, 0, 0.2, 0.4, 0.6, 0.8, 1.0])
+            min_label = bp.np.linspace(min_value, 1,  6)
+            max_label = bp.np.linspace(1, max_value, 6)
+            bias_labels = bp.np.concatenate([min_label[:-1], max_label])
+            
+            # add more stdev labels
+            r_ticks = [0.5, 1.0, 1.5, 2.0, 2.5]
+            
+            for r, move_it in zip(r_ticks, [0.05, 0.09, 0.13, 0.17, 0.21]):
+                # Default side (right/top, angle ~22.5° or wherever your main labels are)
+                # Add labels on the left (angle = À radians)
+                fig.text(0.24 + move_it, 0.119, f"{r}", ha = 'center', va = 'center', fontsize = 9.75, color =  'k')
+                fig.text(0.74 + move_it, 0.119, f"{r}", ha = 'center', va = 'center', fontsize = 9.75, color =  'k')
 
         else:
             diagram.set_thetamin(90)
+            pad = 0.01
             
             # Draw lines for various correlation coefficients as grid
             bias_lines = bp.np.array([0, 0.2, 0.4, 0.6, 0.8, 1.0])
@@ -120,13 +162,13 @@ def bias_taylor(variable_list):
         # Plot all models
         for model, value in zip(MACA_models, data):
             theta = bp.np.pi - bp.np.arccos(value)  # angle for bias
-            pr = one.scatter(theta, gcm_dict[model]['JJA_stdev_ratio'][variable_list[index]], marker = 'o', norm = pr_norm, cmap = bp.cmap.cmap('BlRe'), c = gcm_dict[model]['pr_ratio'], s = 25)
-            ts = two.scatter(theta, gcm_dict[model]['JJA_stdev_ratio'][variable_list[index]], marker = 'o', norm = ts_norm, cmap = bp.cmap.cmap('BlRe'), c = gcm_dict[model]['tasmax_change'], s = 25)
+            pr = one.scatter(theta, gcm_dict[model]['JJA_stdev_ratio'][variable_list[index]], marker = 'o', edgecolors = 'k', linewidths = 0.25, norm = pr_norm, cmap = bp.cmap.cmap('MPL_BrBG'), c = gcm_dict[model]['pr_ratio'], s = 25)
+            ts = two.scatter(theta, gcm_dict[model]['JJA_stdev_ratio'][variable_list[index]], marker = 'o', edgecolors = 'k', linewidths = 0.25, norm = ts_norm, cmap = bp.cmap.cmap('BlRe'), c = gcm_dict[model]['tasmax_change'], s = 25)
 
         for lin, label in zip(bias_lines, bias_labels):
             angle = bp.np.pi - bp.np.arccos(lin)
             diagram.plot([angle, angle], [0, 2.62], '-.', color = 'black', lw = 0.25)
-            diagram.text(angle, 2.74, f"{label:.2f}", ha = 'center', va = 'center', fontsize = 12)
+            diagram.text(angle, 2.8, f"{label:.2f}", ha = 'center', va = 'center', fontsize = 12)
 
         # Set axis limits and labels
         diagram.set_ylim(0, 1.6)
@@ -138,7 +180,7 @@ def bias_taylor(variable_list):
                                orientation = 'horizontal', 
                                ticks = pr_levels, 
                                shrink = 0.75, 
-                               pad = -0.08,
+                               pad = pad,
                                extend = 'both',
                                boundaries = pr_levels)
     pr_cbar.ax.xaxis.set_major_formatter(bp.ticker.FormatStrFormatter('%.0f'))
@@ -147,127 +189,14 @@ def bias_taylor(variable_list):
                                orientation = 'horizontal', 
                                ticks = temp_levels, 
                                shrink = 0.75, 
-                               pad = -0.08,
+                               pad = pad,
                                extend = 'both',
                                boundaries = temp_levels)
     temp_cbar.ax.xaxis.set_major_formatter(bp.ticker.FormatStrFormatter('%.0f'))
 
     bp.plt.show()
     
-    return fig, axs
+    return fig, axs, raw_data
 
 # precip = bias_taylor(['pr', 'pr'])
 temp = bias_taylor(['tasmax', 'tasmax'])
-    
-#%%
-variable_list =  ['pr', 'pr']
-
-# manually set obs data point
-obs_bias = 0
-obs_stdev_ratio = 1
-
-# Setup polar plot: angle = arccos(correlation coefficient), radius = standard deviation
-fig, axs = bp.plt.subplots(1, 2, subplot_kw={'projection': 'polar'}, figsize = (10, 8))
-
-one, two = axs
-
-for index, diagram in enumerate([one, two]):
-    diagram.set_theta_zero_location('W')
-    diagram.set_theta_direction(-1)
-    diagram.set_thetamax(180)
-    diagram.set_xticklabels([])
-    diagram.spines['polar'].set_linewidth(0.5)
-    diagram.spines['polar'].set_edgecolor('black')
-        
-    diagram.xaxis.grid(False)
-
-    # Plot the reference (observation) point 
-    diagram.plot(bp.np.pi - bp.np.arccos(obs_bias), obs_stdev_ratio, 'ko', label = "Reference", markersize = 10)
-
-    min_value, max_value, data = scaled_data(variable_list[index])
-    
-    if min_value < 0:
-        diagram.set_thetamin(0)
-        
-        # Draw lines for various correlation coefficients as grid
-        bias_lines = bp.np.array([-1.0, -0.8, -0.6, -0.4, -0.2, 0, 0.2, 0.4, 0.6, 0.8, 1.0])
-        min_label = bp.np.linspace(min_value, 0,  6)
-        max_label = bp.np.linspace(0, max_value, 6)
-        bias_labels = bp.np.concatenate([min_label[:-1], max_label])
-
-    else:
-        diagram.set_thetamin(90)
-        
-        # Draw lines for various correlation coefficients as grid
-        bias_lines = bp.np.array([0, 0.2, 0.4, 0.6, 0.8, 1.0])
-        bias_labels = bp.np.linspace(min_value, max_value, 6)
-
-    # Plot all models
-    for model, value in zip(MACA_models, data):
-        theta = bp.np.pi - bp.np.arccos(value)  # angle for bias
-        diagram.plot(theta, gcm_dict[model]['JJA_stdev_ratio'][variable_list[index]], 'o', label = model, markersize = 10)
-
-    for lin, label in zip(bias_lines, bias_labels):
-        angle = bp.np.pi - bp.np.arccos(lin)
-        diagram.plot([angle, angle], [0, 2.62], '-.', color = 'black', lw = 0.25)
-        diagram.text(angle, 2.74, f"{label:.2f}", ha = 'center', va = 'center', fontsize = 12)
-
-    # Set axis limits and labels
-    diagram.set_ylim(0, 1.6)
-    diagram.set_yticks([0.5, 1.0, 1.5, 2.0, 2.5])
-    diagram.yaxis.grid(True, color = 'black', linewidth = 0.25, linestyle = '-.')
-    diagram.set_yticklabels([0.5, 1.0, 1.5, 2.0, 2.5], fontsize  = 50)
-    
-    
-# add colorbar
-precip_ratio = bp.np.array([])
-delta_temp = bp.np.array([])
-
-for model in MACA_models:
-    precip_ratio = bp.np.append(precip_ratio, gcm_dict[model]['pr_ratio'])
-    delta_temp = bp.np.append(delta_temp, gcm_dict[model]['tasmax_change'])
-    
-pr_levels = bp.np.linspace(min(precip_ratio), max(precip_ratio), 8)
-
-cmap = bp.plt.get_cmap(bp.cmap.cmap('MPL_BrBG'), 7)
-pr_norm = bp.mcolors.BoundaryNorm(pr_levels, ncolors = cmap.N)
-
-pr_sm = bp.mpl.cm.ScalarMappable(norm = pr_norm, cmap = cmap)
-pr_sm.set_array([])
-
-# shared colorbar for both Taylor diagrams
-pr_cbar = fig.colorbar(
-    pr_sm,
-    ax = one,                
-    orientation='horizontal',
-    extend='both',
-    ticks = pr_levels,
-    boundaries = pr_levels,
-    aspect = 50,
-)
-
-pr_cbar.ax.tick_params(labelsize=20)
-pr_cbar.ax.yaxis.set_major_formatter(bp.ticker.FormatStrFormatter('%.0f'))
-
-
-temp_levels = bp.np.linspace(min(delta_temp), max(delta_temp), 8)
-
-temp_norm = bp.mcolors.BoundaryNorm(temp_levels, ncolors = cmap.N)
-temp_sm = bp.mpl.cm.ScalarMappable(norm = temp_norm, cmap = cmap)
-temp_sm.set_array([])
-
-# shared colorbar for both Taylor diagrams
-temp_cbar = fig.colorbar(
-    temp_sm,
-    ax = two,                
-    orientation = 'horizontal',
-    extend = 'both',
-    ticks = temp_levels,
-    boundaries = temp_levels,
-    aspect = 50,
-)
-
-temp_cbar.ax.tick_params(labelsize=20)
-temp_cbar.ax.yaxis.set_major_formatter(bp.ticker.FormatStrFormatter('%.0f'))
-
-bp.plt.show()
